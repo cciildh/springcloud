96029e4bcbca7cadaa961627f9f6528024327520
cciildh <493830938@qq.com>
2020-08-22 20:31:50 +0800

    eureka

--- product/src/main/java/com/example/ProductApplication.java
+++ product/src/main/java/com/example/ProductApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ProductApplication {
 
     public static void main(String[] args) {
