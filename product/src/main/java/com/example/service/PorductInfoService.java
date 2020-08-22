4c586d9791243e36f0da6c3ee17279d92d7c5c6d
cciildh <493830938@qq.com>
2020-08-22 20:58:29 +0800

    eureka

--- product/src/main/java/com/example/service/PorductInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.entity.ProductInfo;
-
-import java.util.List;
-
-public interface PorductInfoService {
-    List<ProductInfo> getUpAll();
-}
--- product/src/main/java/com/example/service/ProductCategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.entity.ProductCategory;
-
-import java.util.List;
-
-public interface ProductCategoryService {
-    List<ProductCategory> ListGetCategoryAll(List<Integer> categoryTypeList);
-}
