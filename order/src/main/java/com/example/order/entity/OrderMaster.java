4801da78e94c226793255c7fe1ca3b332b324a48
cciildh <493830938@qq.com>
2020-08-22 21:14:34 +0800

    eureka

---
+++ order/src/main/java/com/example/order/entity/OrderMaster.java
@@ -0,0 +1,60 @@
+package com.example.order.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity(name = "order_master")
+@Data
+public class OrderMaster {
+    @Id
+    private String orderId;
+
+    /**
+     * 买家名字
+     */
+    private String buyerName;
+
+    /**
+     * 买家电话
+     */
+    private String buyerPhone;
+
+    /**
+     * 买家地址
+     */
+    private String buyerAddress;
+
+    /**
+     * 买家微信openid
+     */
+    private String buyerOpenid;
+
+    /**
+     * 订单总金额
+     */
+    private BigDecimal orderAmount;
+
+    /**
+     * 订单状态, 默认为新下单
+     */
+    private Byte orderStatus;
+
+    /**
+     * 支付状态, 默认未支付
+     */
+    private Byte payStatus;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+}
