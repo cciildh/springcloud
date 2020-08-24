2deddf53955db9b093186e3fb409e8bae3497912
cciildh <493830938@qq.com>
2020-08-24 19:12:12 +0800

    eureka 服务注册与发现测试

--- order/src/main/java/com/example/order/controller/ClientController.java
+++ order/src/main/java/com/example/order/controller/ClientController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * 测试服务注册与发现
+ */
 @RestController
 public class ClientController {
     @Autowired
@@ -19,7 +22,9 @@ public class ClientController {
 //        String result = restTemplate.getForObject("http://localhost:8081/msg", String.class);
 
         //方式二
+        //使用tspring:application:name: server-name 来获取服务名为product-client的下的服务
         ServiceInstance serviceInstance = loadBalancerClient.choose("product-client");
+        //为product-client中的一个服务
         String url = String.format("http://%s:%s", serviceInstance.getHost(), serviceInstance.getPort() + "/msg");
         String result = restTemplate.getForObject(url, String.class);
         return result;
