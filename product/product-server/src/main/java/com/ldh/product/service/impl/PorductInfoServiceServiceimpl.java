985d1be6d5bba2632dd8a2969794893c4ed2d114
cciildh <493830938@qq.com>
2020-08-27 20:43:12 +0800

    添加rabbitmq 简单使用

--- product/product-server/src/main/java/com/ldh/product/service/impl/PorductInfoServiceServiceimpl.java
+++ product/product-server/src/main/java/com/ldh/product/service/impl/PorductInfoServiceServiceimpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -39,6 +40,7 @@ public class PorductInfoServiceServiceimpl implements PorductInfoService {
 
 
     @Override
+    @Transactional
     public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
         List<ProductInfo> productInfoList = new ArrayList<>();
         for (DecreaseStockInput decreaseStockInput : decreaseStockInputList) {
